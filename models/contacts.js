b088653f4bdaf57cb52347c098c0a1db3ddd0374
Roman-Solovei <romario2409@gmail.com>
2022-05-28 12:55:30 +0300

    hw 02 02

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,16 @@
-// const fs = require('fs/promises')
+const fs = require('fs').promises;
+const path = require('path');
+// const uuid = require('uuid');
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json');
+
+const listContacts = async () => {  
+  const data = await fs.readFile(contactsPath, "utf8");
+  const allContacts = JSON.parse(data);
+  console.table(allContacts);
+  return allContacts;  
+
+}
 
 const getContactById = async (contactId) => {}
 
